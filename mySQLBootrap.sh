f6f34c2cfdfaa45fb551b0e4c374a2d3bdce03b9
Robin Melanson <remcogold@gmail.com>
2019-05-07 14:04:41 -0400

    Update mySQLBootrap.sh

--- mySQLBootrap.sh
+++ mySQLBootrap.sh
@@ -2,9 +2,19 @@
 sqlCurrDir=$PWD
 
 # Ensure script is running under root
+bootstrap=$webCurrDir/mySQLBootrap.sh
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root or under sudo"
-  return -1
+then
+   sudo -n true 2/dev/null 2>&1
+   passwordRequired=$?
+
+   if [ "$passwordRequired" == "1" ]; then
+       echo "Please run as root or under user with sudo access sudo"
+   else
+       sudo chmod +x $bootstrap
+       sudo $bootstrap
+   fi
+   return 1
 fi
 
 #Initial update of server
@@ -38,4 +48,4 @@ find . -name "*.sh" -exec chmod 700 {} \;
 # Setup Project
 ./setup.sh 2>&1| tee setup.log
 
-cd $sqlCurrDir
+cd $bootstrapDir
